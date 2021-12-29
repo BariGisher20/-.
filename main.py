9e997e0b5f295013f8499c58a0fa4592f28f381a
Eleonora Harutyunyan <ev.arutyunyan@mail.ru>
2021-12-29 18:08:20 +0300

    Task 1

---
+++ main.py
@@ -0,0 +1,42 @@
+from typing import Any
+from pprint import pprint
+
+
+nested_list = [
+ ['a', 'b', 'c'],
+ ['d', 'e', 'f', 'h', False],
+ [1, 2, None],
+]
+
+
+class MyList(list):
+    def __iter__(self):
+        self.main_cursor = -1
+        return self
+
+    def __next__(self):
+        self.main_cursor += 1
+        if len(self) == self.main_cursor:
+            raise StopIteration
+        return self[self.main_cursor]
+
+
+my_list = MyList(nested_list)
+
+
+class Item(list):
+    def __iter__(self):
+        self.inner_cursor = -1
+        return self
+
+    def __next__(self):
+        self.inner_cursor += 1
+        if len(self) == self.inner_cursor:
+            raise StopIteration
+        return str(self[self.inner_cursor])
+
+
+for item in my_list:
+    my_item = Item(item)
+    for i in item:
+        pprint(i)
