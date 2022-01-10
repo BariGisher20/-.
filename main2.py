f5ecac3258c09dff1a987caf9eb04f176a10210e
Eleonora Harutyunyan <ev.arutyunyan@mail.ru>
2022-01-10 22:00:14 +0300

    Task 2

--- main2.py
+++ main2.py
@@ -4,14 +4,17 @@ from pprint import pprint
 nested_list = [
  ['a', 'b', 'c'],
  ['d', 'e', 'f', 'h', False],
- [1, 2, None],
+ [1, 2, None]
 ]
 
 
-def flat_generator(a: list) -> list:
-    for item in a:
-        for i in item:
-            return i
+def flat_generator(main_list):
+    for item in main_list:
+        if isinstance(item, list):
+            for value in flat_generator(item):
+                yield value
+        else:
+            yield item
 
 
 for item in flat_generator(nested_list):
